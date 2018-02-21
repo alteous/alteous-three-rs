d80f558fad880b40f8e13cccae723598d5b091f2
David Harvey-Macaulay <alteous@outlook.com>
2018-02-21 21:41:20 +0000

    Update materials example

--- examples/materials.rs
+++ examples/materials.rs
@@ -1,49 +1,28 @@
+#[macro_use]
+extern crate euler;
 extern crate three;
 
 use three::Object;
 
 fn main() {
-    let mut win = three::Window::new("Three-rs materials example");
-    let mut cam = win.factory.perspective_camera(75.0, 1.0 .. 50.0);
-    cam.set_position([0.0, 0.0, 10.0]);
+    let (mut window, mut input, mut renderer, mut factory) = three::init();
 
-    let mut light = win.factory.point_light(0xffffff, 0.5);
-    let mut pos = [0.0, 5.0, 5.0];
+    let mut scene = factory.scene();
+    
+    let camera = factory.perspective_camera(75.0, 1.0 .. 50.0);
+    camera.set_position(vec3!(0.0, 0.0, 10.0));
+
+    let light = factory.point_light(0xffffff, 0.5);
+    let mut pos = vec3!(0.0, 5.0, 5.0);
     light.set_position(pos);
-    light.set_parent(&win.scene);
+    scene.add(&light);
 
     let geometry = three::Geometry::cylinder(1.0, 2.0, 2.0, 5);
     let mut materials: Vec<three::Material> = vec![
-        three::material::Basic {
-            color: 0xFFFFFF,
-            map: None,
-        }.into(),
-        three::material::Lambert {
-            color: 0xFFFFFF,
-            flat: true,
-        }.into(),
-        three::material::Lambert {
-            color: 0xFFFFFF,
-            flat: false,
-        }.into(),
-        three::material::Phong {
-            color: 0xFFFFFF,
-            glossiness: 80.0,
-        }.into(),
-        three::material::Pbr {
-            base_color_factor: 0xFFFFFF,
-            base_color_alpha: 1.0,
-            metallic_factor: 0.5,
-            roughness_factor: 0.5,
-            occlusion_strength: 0.2,
-            emissive_factor: 0x000000,
-            normal_scale: 1.0,
-            base_color_map: None,
-            normal_map: None,
-            emissive_map: None,
-            metallic_roughness_map: None,
-            occlusion_map: None,
-        }.into(),
+        three::material::Basic { color: 0xFFFFFF, map: None }.into(),
+        three::material::Lambert { color: 0xFFFFFF }.into(),
+        three::material::Gouraud { color: 0xFFFFFF }.into(),
+        three::material::Phong { color: 0xFFFFFF, glossiness: 80.0 }.into(),
     ];
     let count = materials.len();
 
@@ -52,19 +31,20 @@ fn main() {
         .enumerate()
         .map(|(i, mat)| {
             let offset = 4.0 * (i as f32 + 0.5 - 0.5 * count as f32);
-            let mut mesh = win.factory.mesh(geometry.clone(), mat);
-            mesh.set_position([offset, 0.0, 0.0]);
-            mesh.set_parent(&win.scene);
+            let mesh = factory.mesh(geometry.clone(), mat);
+            mesh.set_position(vec3!(offset, 0.0, 0.0));
+            scene.add(&mesh);
             mesh
         })
         .collect();
 
-    while win.update() && !win.input.hit(three::KEY_ESCAPE) {
-        if let Some(diff) = win.input.timed(three::AXIS_LEFT_RIGHT) {
-            pos[0] += 5.0 * diff;
+    while !input.quit_requested() && !input.hit(three::KEY_ESCAPE) {
+        input.update();
+        if let Some(diff) = input.timed(three::AXIS_LEFT_RIGHT) {
+            pos.x += 5.0 * diff;
             light.set_position(pos);
         }
-
-        win.render(&cam);
+        renderer.render(&scene, &camera, &window);
+        window.swap_buffers();
     }
 }
